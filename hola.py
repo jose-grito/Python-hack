867c92429c6d7e9830f5a0909701076e57982073
hack <danny_123_46@hotmail.com>
2026-01-28 17:26:39 -0500

    Actualizar contenido de hola.py

--- hola.py
+++ hola.py
@@ -1,10 +1,1 @@
-nota=int(input("ingrese la nota: "))
-
-if nota>=80:
-   print("Excelente")
-elif nota>=70:
-   print("regular")
-elif nota>=68:
-   print("intentalo")
-else:
-   print("desaprobado")
+print("hola mundo")
