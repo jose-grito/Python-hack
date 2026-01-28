46a60791b942b37bf5a39cf03998e29f92bd20d7
hack <danny_123_46@hotmail.com>
2026-01-27 19:30:46 -0500

    Nuevo mensaje para el commit

---
+++ if.py
@@ -0,0 +1,10 @@
+nota=int(input("ingrese la nota: "))
+
+if nota>=80:
+   print("Excelente")
+elif nota>=70:
+   print("regular")
+elif nota>=68:
+   print("intentalo")
+else:
+   print("desaprobado")
